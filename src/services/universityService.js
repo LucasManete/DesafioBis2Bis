70805eb6874e6ab186d0806c36c6228bd604a5ac
LucasManete <lucas.manete2403@gmail.com>
2022-09-20 20:28:20 -0300

    refact: service

--- src/services/universityService.js
+++ src/services/universityService.js
@@ -1,71 +1,74 @@
 const { notFoundException, conflictException, sucessException } = require('../HttpCatalog/httpCatalog');
+const formatUniversities = require('../utils/formatAPI');
 const api = require('../requestApi/request');
-const formatUniversities = require('../utils/formatUniversities');
+const model = require('../schemas/universitySchema');
 
-class ApiService {
-  constructor(model) {
-    this.model = model;
+const getAll = async (query) => {
+  const { page = 1, limit = 20, country } = query;
+  const options = { page, limit, select: ['_id', 'name', 'country', 'state_province'] };
+  const filters = {};
+
+  if (limit > 20) {
+    options.limit = 20;
   }
 
-  async getAll(query) {
-    const { page = 1, limit = 20, country } = query;
-    const options = { page, limit, select: ['_id', 'name', 'country', 'state_province'] };
-    const filters = {};
-
-    if (limit > 20) {
-      options.limit = 20;
-    }
-
-    if (country) {
-      Object.assign(filters, { country });
-    }
-    const result = await this.model.paginate(filters, options);
-    return result;
+  if (country) {
+    Object.assign(filters, { country });
   }
+  const result = await model.paginate(filters, options);
 
-  async getOne({ id }) {
-    const result = await this.model.findById(id);
-    if (!result) { return notFoundException('Universidade não econtrada'); }
-    return sucessException(result);
+  return sucessException(result);
+};
+
+const getOne = async ({ id }) => {
+  const result = await model.findById(id);
+  if (!result) { return notFoundException('Universidade não econtrada'); }
+  return sucessException(result);
+};
+
+const create = async (body) => {
+  const findUniversity = await model.findOne({
+    name: body.name,
+    country: body.country,
+    state_province: body.state_province,
+  });
+
+  if (findUniversity) { return conflictException('Universidade já cadastrada'); }
+
+  const result = await model.create(body);
+  return sucessException(result);
+};
+
+const update = async (id, body) => {
+  const university = await model.findByIdAndUpdate(id, body);
+  if (!university) {
+    return notFoundException('Universidade não econtrada');
   }
+  return sucessException('Dados atualizados com sucesso');
+};
 
-  async create(body) {
-    const findUniversity = await this.model.findOne({
-      name: body.name,
-      country: body.country,
-      state_province: body.state_province,
-    });
-
-    if (findUniversity) { return conflictException('Universidade já cadastrada'); }
-
-    const result = await this.schema.create(body);
-    return sucessException(result);
+const destroy = async (id) => {
+  const university = await model.findByIdAndDelete(id);
+  if (!university) {
+    return notFoundException('Universidade não econtrada');
   }
+  return sucessException('Universidade Deletada com sucesso');
+};
 
-  async update({ id }, body) {
-    const university = await this.model.findByIdAndUpdate(id, body);
-    if (!university) {
-      return notFoundException('Universidade não econtrada');
-    }
-    return sucessException('Dados atualizados com sucesso');
-  }
+const populate = async () => {
+  const verifyBD = await model.find();
+  if (verifyBD.length > 0) return conflictException('Banco já populado');
+  const request = await api.requestAPI();
+  const formated = await formatUniversities(request);
+  const result = await model.insertMany(formated);
+  return sucessException(`Banco foi polulado com ${result.length} registros`);
+};
 
-  async delete({ id }) {
-    const university = await this.model.findByIdAndDelete(id);
-    if (!university) {
-      return notFoundException('Universidade não econtrada');
-    }
-    return sucessException('Universidade Deletada com sucesso');
-  }
-
-  async populate() {
-    const verifyBD = await this.model.find();
-    if (verifyBD.length > 60) return conflictException('Banco já populado');
-    const request = await api.requestAPI();
-    const formated = await formatUniversities(request);
-    const result = await this.model.insertMany(formated);
-    return sucessException(`O banco foi polulado com ${result.length} resultados`);
-  }
-}
-
-module.exports = ApiService;
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy,
+  populate,
+};
